77e851da5ad3d7e810de1a02430d766c54e2ec83
daniel <danielfelipesachez23rgmail.com>
2022-12-13 13:29:41 -0500

    trabajo

---
+++ app_blog/blog/views.py
@@ -0,0 +1,8 @@
+from django.views.generic import ListView
+from .models import Post
+from django.shortcuts import render
+
+# Create your views here.
+class BlogListView(ListView):
+    model = Post
+    template_name = "home.html"
